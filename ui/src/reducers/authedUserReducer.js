98ecb537ac80a93e39b94171727efcba83e479dc
Olga Lushnikova <63054910+basto13@users.noreply.github.com>
2022-01-26 21:45:10 +0200

    Update authedUserReducer.js

--- ui/src/reducers/authedUserReducer.js
+++ ui/src/reducers/authedUserReducer.js
@@ -1,13 +1,11 @@
 import {SET_AUTHED_USER} from "../actions/authedUser"
 import { LOG_OUT_USER } from "../actions/logoutUser"
 
-export default function authedUser(state = JSON.parse(localStorage.getItem('authUser')), action) {
+export default function authedUser(state = null, action) {
     switch (action.type) {
         case SET_AUTHED_USER:
-            localStorage.setItem('authUser', JSON.stringify(action.user))
             return action.user
         case LOG_OUT_USER:
-            localStorage.removeItem('authUser')
             return null
         default:
             return state
